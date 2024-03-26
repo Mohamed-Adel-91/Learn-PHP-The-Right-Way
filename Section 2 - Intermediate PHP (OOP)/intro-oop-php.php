21d09a927a1bbba1aad6f14cd65c52a8c299f11e
MohamedAdel <mohamed101291@gmail.com>
2024-03-26 21:04:30 +0200

    oop

--- Section 2 - Intermediate PHP (OOP)/intro-oop-php.php
+++ Section 2 - Intermediate PHP (OOP)/intro-oop-php.php
@@ -1,1 +1,23 @@
 <?php
+
+/*
+ * oop != mvc
+ * oop  => Encapsulation
+ *      => Abstraction
+ *      => Inheritance
+ *      => Polymorphism
+ * ---------------------------------------------
+ * topics in this section :=
+ * Object & Classes
+ * Magic methods
+ * Code style & PSR
+ * Name spacing , Autoloading & Composer
+ * Encapsulation
+ * Abstraction
+ * Inheritance
+ * Polymorphism
+ * Traits & Statics
+ * Super Global Variables ($_POST,...)
+ * Cookies & Sessions
+ * Database
+ */
